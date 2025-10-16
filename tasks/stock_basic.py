1b67ce7af6a2cf409549897a24c69669b3aa3d5f
hongli <hongli.wang@ishansong.com>
2025-10-16 20:45:02 +0800

    newtable

--- tasks/stock_basic.py
+++ tasks/stock_basic.py
@@ -5,33 +5,41 @@ from core.logger import logger
 from utils.pinyin import PinyinConverter
 from utils.date_utils import get_timestamp_ms
 
-def run_stock_basic():
-    logger.info("正在从 AkShare 获取股票基础信息...")
-    
-    df = ak.stock_info_a_code_name()
-    logger.info(f"获取到 {len(df)} 条数据")
+class StockBasicInfo:
 
-    if len(df) == 0:
-        logger.error("获取数据失败，请检查 AkShare 配置")
-        return
+    def saveOrUpdateStockBaseInfo():
+        logger.info("正在从 AkShare 获取股票基础信息...")
+        
+        df = ak.stock_info_a_code_name()
 
-    sql = """
-    INSERT INTO stock_base_info (code, name, pinyin, create_time, update_time)
-    VALUES (%s, %s, %s, %s, %s)
-    ON DUPLICATE KEY UPDATE
-        name = VALUES(name),
-        pinyin = VALUES(pinyin),
-        update_time = VALUES(update_time);
-    """
+        if len(df) == 0:
+            logger.error("获取数据失败，请检查 AkShare 配置")
+            return
 
-    timeStampMs = get_timestamp_ms()
-    converter = PinyinConverter()
+        sql = """
+        INSERT INTO stock_base_info (code, name, pinyin, create_time, update_time)
+        VALUES (%s, %s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE
+            name = VALUES(name),
+            pinyin = VALUES(pinyin),
+            update_time = VALUES(update_time);
+        """
 
-    data = [(row["code"], row["name"],converter.hanzi_to_pinyin(row["name"],mode="first"),timeStampMs,timeStampMs) for _, row in df.iterrows()]
-    storage = StorageFactory.get_storage(STORAGE_TYPE, DATABASE)
-    storage.insert_many(sql, data)
-    
-    logger.info(f" 已写入 MySQL，共 {len(data)} 条。")
+        timeStampMs = get_timestamp_ms()
+        converter = PinyinConverter()
+
+        data = [(
+                row["code"], 
+                converter.clean_stock_name(row["name"]),
+                converter.hanzi_to_pinyin(row["name"],mode="first"),
+                timeStampMs,timeStampMs) 
+                for _, row in df.iterrows()
+            ]
+        
+        storage = StorageFactory.get_storage(STORAGE_TYPE, DATABASE)
+        storage.insert_many(sql, data)
+        
+        logger.info(f" 已写入 MySQL,共 {len(data)} 条。")
     
 
     
